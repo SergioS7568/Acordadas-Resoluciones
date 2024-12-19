13f5660c449afaeb764cd2f4faacb8e25519ae2c
SergioS7568 <sergio7568@hotmail.com>
2024-12-19 07:34:41 -0300

    header is now using css file extention with tailwind custom styles

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,13 @@
+import "./Header.css";
 const Header = () => {
   const imageLogoURL =
     "https://acordadas.justucuman.gov.ar/img/logos/logo-corte.png";
   const modName = "Acordadas y Resoluciones";
   return (
     <div>
-      <label className="flex flex-row justify-between ">
-        <img src={imageLogoURL} className="max-h-80 max-w-96" /> {modName}
+      <label className="TW_HeaderStyle">
+        <img src={imageLogoURL} className="TW_HeaderImageProperties" />
+        {modName}
       </label>
     </div>
   );
