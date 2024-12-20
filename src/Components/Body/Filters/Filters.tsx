4f4f5acaffe3912c8cd04544b02f60936c0108e0
SergioS7568 <sergio7568@hotmail.com>
2024-12-20 10:25:21 -0300

    feat: validation now corrects spacing in both the text input and number input, grid removed, using tailwind responsive design

--- src/Components/Body/Filters/Filters.tsx
+++ src/Components/Body/Filters/Filters.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 
 import CustomButton from "../../Buttons/CustomButton";
-import Validation from "../../../Lib/Validation";
+import validation from "../../../Lib/Validation";
 
 export interface DataType {
   number: string | null;
@@ -25,9 +25,8 @@ const Filters = () => {
   });
 
   const handleOnSubmit = (data: DataType) => {
-    const isValid = Validation({ dataSniffed: data });
+    const isValid = validation({ dataSniffed: data });
     if (!isValid) {
-      console.log("erronous data :", data);
       return;
     }
     console.log("data submitted :", data);
@@ -36,34 +35,69 @@ const Filters = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(handleOnSubmit)}>
-        <input id="Numero" {...register("number")} placeholder="Número"></input>
-        <input
-          type="date"
-          id="init-date"
-          {...register("init-date", { valueAsDate: true })}
-          placeholder="dd/mm/yyyy”"
-        ></input>
-        <input
-          type="date"
-          id="final-day"
-          {...register("final-day", { valueAsDate: true })}
-          placeholder="dd/mm/yyyy”"
-        ></input>
-        <input
-          id="Texto"
-          type="text"
-          {...register("text")}
-          placeholder="Texto"
-        ></input>
-        <select id="Tipo" typeof="string" {...register("type")}>
-          <option disabled hidden value={""}>
-            Tipo
-          </option>
-          <option value={"ACORDADA"}>ACORDADA</option>
-          <option value={"RESOLUCION"}>RESOLUCION</option>
-          <option value={"RESOLUCION DE FERIA"}>RESOLUCION DE FERIA</option>
-        </select>
-        <div>
+        <div className=" flex flex-col md:flex-row justify-center items-center gap-4 ">
+          <div className="flex flex-col    text-sm font-medium  ">
+            <span>numero</span>
+            <input
+              className="input outline w-96 md:w-60 "
+              id="Numero"
+              {...register("number")}
+              placeholder="Número"
+            ></input>
+          </div>
+          <div className=" flex flex-row gap-4">
+            <div className="flex flex-col    text-sm font-medium  ">
+              <span>Fecha Inicial</span>
+              <input
+                className="input outline   w-60"
+                type="date"
+                id="init-date"
+                {...register("init-date", { valueAsDate: true })}
+                placeholder="dd/mm/yyyy”"
+              ></input>
+            </div>
+            <div className="flex flex-col    text-sm font-medium  ">
+              <span>Fecha Final</span>
+              <input
+                className="input outline   w-60 "
+                type="date"
+                id="final-day"
+                {...register("final-day", { valueAsDate: true })}
+                placeholder="dd/mm/yyyy”"
+              ></input>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row justify-center gap-4">
+          <div className="flex flex-col    text-sm font-medium  ">
+            <span>Texto</span>
+            <input
+              className="input outline  w-60 "
+              id="Texto"
+              type="text"
+              {...register("text")}
+              placeholder="Texto"
+            ></input>
+          </div>
+          <div className="flex flex-col    text-sm font-medium  ">
+            <span>Tipo</span>
+            <select
+              className="select outline  w-60"
+              id="Tipo"
+              typeof="string"
+              {...register("type")}
+            >
+              <option disabled hidden value={""}>
+                Tipo
+              </option>
+              <option value={"ACORDADA"}>ACORDADA</option>
+              <option value={"RESOLUCION"}>RESOLUCION</option>
+              <option value={"RESOLUCION DE FERIA"}>RESOLUCION DE FERIA</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center gap-4">
           <CustomButton action={"submit"} reset={null} register={register} />
           <CustomButton action={"reset"} reset={reset} register={null} />
         </div>
--- src/Lib/Validation.tsx
+++ src/Lib/Validation.tsx
@@ -12,56 +12,45 @@ interface Props {
   dataSniffed: DataType;
 }
 
-export const Validation = (props: Props): boolean => {
+export const validation = (props: Props): boolean => {
   const { dataSniffed } = props;
 
   const regex =
     /^$|^\d{1}(\/|-)\d{2,4}$|^\d{3,25}$|^\d{2,25}(\/|-)\d{1,4}$|^\d{1,25}(\/|-)\d{2,4}$|^\d{3,25}(\/|-)$/;
 
   const schema: z.ZodType<DataType> = z.object({
-    number: z
-      .string()
-      .regex(regex)
-
-      .nullable(),
-
+    number: z.string().trim().regex(regex).nullable(),
     "init-date": z.date().nullable(),
     "final-day": z.date().nullable(),
-    text: z.string().nullable(),
+    text: z.string().trim().nullable(),
     type: z.string().nullable(),
   });
-
-  console.log("Number field value:", dataSniffed.number);
-  console.log("Number field value:", dataSniffed.text);
 
   const result = schema.safeParse(dataSniffed);
 
   // Log the result (you can also display it if needed)
 
-  if (dataSniffed.number?.trim() && !regex.test(dataSniffed.number)) {
+  if (dataSniffed.number?.trim() && !regex.test(dataSniffed.number.trim())) {
     alert("wrong numbers! please try again");
     return false;
   }
-  if (dataSniffed.number?.trim() && dataSniffed.number.trim.length < 3) {
-    alert("Too short! increase numbers");
-    return false; // return false indicating failure
-  }
 
   if (dataSniffed.text?.trim() && dataSniffed.text.trim().length < 3) {
     alert(". Text is too short");
-    return false; // return false indicating failure
+    return false;
   }
 
-  if (dataSniffed.text?.trim() && dataSniffed.text.trim().length < 3) {
-    alert(". Text is too short");
-    return false; // return false indicating failure
+  if (dataSniffed["final-day"] && dataSniffed["init-date"]) {
+    if (dataSniffed["final-day"] < dataSniffed["init-date"]) {
+      alert(". end date cannot be shorter than starting date");
+      return false;
+    }
   }
 
   if (!result.success) {
     console.log("Validation failed:", result.error.format());
     return false;
   }
-
   return true;
 };
-export default Validation;
+export default validation;
