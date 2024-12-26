b2801f6c1ba0233948ab58ba4e91573057ac2f36
SergioS7568 <sergio7568@hotmail.com>
2024-12-26 10:30:27 -0300

    feat: number and year function added for the numero input

--- src/Lib/getAgreements.tsx
+++ src/Lib/getAgreements.tsx
@@ -15,42 +15,87 @@ export const getUsersInfoFilter = async (
 ) => {
   const [dataUnfiltered, pageSize] = queryKey;
 
-  const formattedDateFinal = `${dataUnfiltered["final-day"]?.getFullYear()}-${(
-    dataUnfiltered["final-day"]?.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, "0")}-${dataUnfiltered["final-day"]
-    ?.getDate()
-    .toString()
-    .padStart(2, "0")}`;
+  const dateAll = new Date();
+  const year = dateAll.getFullYear();
+  const yearwithoutTwoDigit = year.toString().substring(2);
+  const yearwithoutThreeDigit = year.toString().substring(1);
 
-  const formattedDateInit = `${dataUnfiltered["init-date"]?.getFullYear()}-${(
-    dataUnfiltered["init-date"]?.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, "0")}-${dataUnfiltered["init-date"]
-    ?.getDate()
-    .toString()
-    .padStart(2, "0")}`;
+  const changeNumberFormat = (text: string | null) => {
+    let wantedNumber = "";
+    const fowardSlash = text?.split("/");
+
+    if (fowardSlash && fowardSlash[0].trim().length == 2) {
+      if (
+        parseFloat(fowardSlash[0]) &&
+        parseFloat(fowardSlash[0]) <= parseFloat(yearwithoutTwoDigit)
+      ) {
+        wantedNumber += `20${fowardSlash[0]}`;
+      } else {
+        wantedNumber += `19${fowardSlash[0]}`;
+      }
+
+      return wantedNumber;
+    }
+
+    if (fowardSlash && fowardSlash[0].trim().length == 3) {
+      if (
+        parseFloat(fowardSlash[0]) &&
+        parseFloat(fowardSlash[0]) <= parseFloat(yearwithoutThreeDigit)
+      ) {
+        wantedNumber += `2${fowardSlash[0]}`;
+      } else {
+        wantedNumber += `1${fowardSlash[0]}`;
+      }
+      return wantedNumber;
+    }
+  };
 
   let filterSearch = "";
   if (pageSize && pageSize > 0) {
-    filterSearch += `?size=${pageSize}&index=0`;
+    filterSearch += `size=${pageSize}&index=0`;
     if (
       dataUnfiltered["final-day"] &&
       dataUnfiltered["final-day"].toString().trim()
-    )
+    ) {
+      const formattedDateFinal = `${dataUnfiltered[
+        "final-day"
+      ]?.getFullYear()}-${(dataUnfiltered["final-day"]?.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${dataUnfiltered["final-day"]
+        ?.getDate()
+        .toString()
+        .padStart(2, "0")}`;
+
       filterSearch += `&final-day=lessOrEquals>${formattedDateFinal}`;
+    }
   }
   if (
     dataUnfiltered["init-date"] &&
     dataUnfiltered["init-date"].toString().trim()
   ) {
-    filterSearch += `&init-date=greaterOrEquals>${formattedDateInit}`;
+    {
+      const formattedDateInit = `${dataUnfiltered[
+        "init-date"
+      ]?.getFullYear()}-${(dataUnfiltered["init-date"]?.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${dataUnfiltered["init-date"]
+        ?.getDate()
+        .toString()
+        .padStart(2, "0")}`;
+      filterSearch += `&init-date=greaterOrEquals>${formattedDateInit}`;
+    }
   }
 
   if (dataUnfiltered.number && dataUnfiltered.number?.trim()) {
-    filterSearch += `&number=equals>${dataUnfiltered.number}`;
+    // &number=equals>1&year=equals>2020
+    const numberValuePage = dataUnfiltered.number.split("/");
+
+    filterSearch += `&number=equals>${numberValuePage[0]}`;
+
+    if (numberValuePage[1] && numberValuePage[1].toString().trim()) {
+      const formattedYear = changeNumberFormat(numberValuePage[1]);
+      filterSearch += `&year=equals>${formattedYear}`;
+    }
   }
   if (dataUnfiltered.text && dataUnfiltered.text?.trim()) {
     filterSearch += `&text=contains>${dataUnfiltered.text}`;
@@ -66,9 +111,9 @@ export const getUsersInfoFilter = async (
       filterSearch += `&type=equals>RF`;
     }
   }
-  console.log("filterSearch    ", filterSearch.toString());
 
-  const urlAcordadas = `${BACKEND_URL}`;
+  const urlAcordadas = `${BACKEND_URL}${filterSearch.toString()}`;
+  console.log("urlAcordadas   ", urlAcordadas);
 
   const resultado = await fetch(urlAcordadas);
 
