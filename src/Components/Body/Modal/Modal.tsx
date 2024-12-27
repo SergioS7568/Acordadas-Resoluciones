ba87ffe79683405e00381c4dc5fc417939662874
SergioS7568 <sergio7568@hotmail.com>
2024-12-27 07:13:47 -0300

    feat: showing basic data and rtftodiv correctly in the modal

--- src/Components/Body/Modal/Modal.tsx
+++ src/Components/Body/Modal/Modal.tsx
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import rtf from "rtf.js";
 
 import "./Modal.css";
 
 import { getTextInfo } from "../../../Lib/getAgreements";
+import highlight from "../../../Lib/highlight";
+import rtfToDiv from "../../../Lib/rtfToDiv";
 
 interface Props {
   isOpen: boolean;
@@ -26,6 +30,33 @@ const Modal = (props: Props) => {
     enabled: !!id,
   });
 
+  const [flagLoad, setFlagLoad] = useState<boolean>(false);
+  const [parsedText, setParsedText] = useState<string>("");
+
+  useEffect(() => {
+    const rtfToDivHandler = async () => {
+      if (agreementInformation?.agreement_text) {
+        setFlagLoad(true);
+        try {
+          const htmlContent = await rtfToDiv(
+            agreementInformation.agreement_text,
+            "Poder"
+          );
+          // If rtfToDiv isn't working as expected, you could try rtf.js instead:
+          // const htmlContent = parseRtf(agreementInformation.agreement_text);
+          const highlightedText = highlight(htmlContent, "Poder");
+          console.log("Parsed Text:", highlightedText);
+          setParsedText(highlightedText);
+        } catch (error) {
+          alert(error);
+        } finally {
+          setFlagLoad(false);
+        }
+      }
+    };
+    rtfToDivHandler();
+  }, [agreementInformation]);
+
   return (
     <>
       {props.isOpen && (
@@ -43,6 +74,7 @@ const Modal = (props: Props) => {
             <p>{agreementInformation?.agreement_description}</p>
             <p>{agreementInformation?.agreement_number}</p>
             <p>{agreementInformation?.agreement_text}</p>
+            <div></div>
             <p>{agreementInformation?.agreement_year}</p>
             <p>{agreementInformation?.file_number}</p>
             <p>{agreementInformation?.id}</p>
@@ -56,6 +88,9 @@ const Modal = (props: Props) => {
               }
             )}
 
+            {parsedText && (
+              <div dangerouslySetInnerHTML={{ __html: parsedText }} />
+            )}
             <button className="btn btn-circle">list</button>
             <button className="btn btn-circle">left</button>
             <button className="btn btn-circle">right</button>
