1231e04675201ef20b7641e78f3684479148cf26
SergioS7568 <sergio7568@hotmail.com>
2024-12-29 09:40:58 -0300

    feat: new variables types and functions to help us to move between pages and elements, were added in the zustand store

--- src/store.ts
+++ src/store.ts
@@ -4,12 +4,20 @@ import { DataType } from "./Lib/getAgreements";
 type StoredType = {
   data: DataType;
   pageSize: number;
+  pageIndex: number;
+  agreementID: number | null;
+  agreementIDList: number[];
+  queryIndex: number;
 };
 
 type Action = {
   updateData: (data: StoredType["data"]) => void;
   cleanSearch: (data: StoredType["data"]) => void;
   updatePageSize: (number: StoredType["pageSize"]) => void;
+  updateQueryIndex: (data: StoredType["queryIndex"]) => void;
+  updatePageIndex: (data: StoredType["pageIndex"]) => void;
+  updateAgreementID: (data: StoredType["agreementID"]) => void;
+  updateIdList: (data: StoredType["agreementIDList"]) => void;
 };
 
 export const useDataType = create<StoredType & Action>((set) => ({
@@ -21,8 +29,16 @@ export const useDataType = create<StoredType & Action>((set) => ({
     type: "",
   },
   pageSize: 10,
-
+  pageIndex: 0,
+  agreementID: null,
+  agreementIDList: [],
+  queryIndex: 0,
   updateData: (data) => set(() => ({ data: data })),
   cleanSearch: (data: DataType) => set(() => ({ data: data })),
   updatePageSize: (number) => set(() => ({ pageSize: number })),
+  updateQueryIndex: (number) => set(() => ({ queryIndex: number })),
+  updatePageIndex: (number) => set(() => ({ pageIndex: number })),
+  updateAgreementID: (number) => set(() => ({ agreementID: number })),
+  updateIdList: (newAgreementIDList) =>
+    set(() => ({ agreementIDList: newAgreementIDList })),
 }));
