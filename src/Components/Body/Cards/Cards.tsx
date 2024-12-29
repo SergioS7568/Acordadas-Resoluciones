d74c6963b96a9ec6843c4f0c969991d3a4079fea
SergioS7568 <sergio7568@hotmail.com>
2024-12-29 09:57:21 -0300

    feat: cards component. screen only shows when the screen size is small

---
+++ src/Components/Body/Cards/Cards.tsx
@@ -0,0 +1,57 @@
+import { AgreementWantedFormat } from "../../../Lib/agreementsFormatConversion";
+import Grid from "../../../Lib/Grid";
+
+interface Props {
+  ApiUser: AgreementWantedFormat | undefined;
+  HandleButtonPress: (id: number, index: number) => void;
+}
+
+const Cards = (props: Props) => {
+  const { ApiUser, HandleButtonPress } = props;
+  return (
+    <div className="block md:hidden lg:hidden m-2">
+      {ApiUser?.agreementsFormat.map((ApiResult, index: number) => {
+        return (
+          <div
+            key={ApiResult.id}
+            className="flex flex-col bg-gray-100 dark:bg-darkGrayOption-0 pt-3 pb-3 pr-6 pl-6 rounded-2xl text-lg gap-4 m-4"
+          >
+            <Grid container>
+              <Grid item xs={12} className="flex flex-row justify-between">
+                <p className="text-start    font-normal pt-2">
+                  {ApiResult.agreement_number}/{ApiResult.agreement_year}
+                </p>
+                <p className="text-end  font-normal pt-2">
+                  {ApiResult.agreement_date.toString()}
+                </p>
+              </Grid>
+              <Grid item xs={12}>
+                <p className="text-start font-bold truncate overflow-hidden whitespace-nowrap ">
+                  {ApiResult.type_agreement.description}
+                </p>
+              </Grid>
+              <Grid item xs={12}>
+                <div
+                  className=" bg-gradient-to-r from-gray-400 to-gray-300 "
+                  style={{ height: "1px" }}
+                ></div>
+              </Grid>
+              <Grid item xs={12}>
+                <p className="text-start  font-normal pb-2 ">
+                  {ApiResult.agreement_description}
+                </p>
+              </Grid>
+              <Grid item xs={12}>
+                <button
+                  className="btn btn-circle"
+                  onClick={() => HandleButtonPress(ApiResult.id, index)}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Cards;
