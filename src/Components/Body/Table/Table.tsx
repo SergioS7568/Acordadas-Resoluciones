d1142bb453fa4cf98f8bc56420ded0fc405149be
SergioS7568 <sergio7568@hotmail.com>
2024-12-30 01:49:12 -0300

    feat: styles

--- src/Components/Body/Table/Table.tsx
+++ src/Components/Body/Table/Table.tsx
@@ -8,6 +8,7 @@ import Modal from "../Modal/Modal";
 
 import PageNavigationButtonsBottom from "../../PageNavigationButtons/PageNavigationButtonsBottom";
 import Cards from "../Cards/Cards";
+import CustomButton from "../../Buttons/CustomButton";
 
 const Table = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +17,6 @@ const Table = () => {
   const newPageSize = useDataType((state) => state.pageSize);
   const UpdatePageSize = useDataType((state) => state.updatePageSize);
 
-  const currentIndex = useDataType((state) => state.queryIndex);
   const setQueryIndex = useDataType((state) => state.updateQueryIndex);
 
   const setPageIndex = useDataType((state) => state.updatePageIndex);
@@ -55,10 +55,8 @@ const Table = () => {
   };
 
   const HandleButtonPress = (id: number, index: number) => {
-    console.log("  ", index);
-    console.log("  ", currentIndex);
     setQueryIndex(index);
-    console.log("  ", currentIndex);
+
     setIDSearch(id);
 
     const selectID = ApiUser?.agreementsFormat.map(({ id }) => id);
@@ -74,117 +72,151 @@ const Table = () => {
   };
 
   return (
-    <div>
-      {isLoading ? (
-        <div className="flex flex-col ">
-          <span className="loading loading-dots w-32 self-center text-lightBlueShift-0 "></span>
-        </div>
-      ) : isError ? (
-        <p className="pl-3 rounded-lg  bg-red-100 text-darkGrayOption-0 min-h-16 text-start items-center flex flex-row gap-1 text-lg font-semibold ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6 text-red-900"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-            />
-          </svg>
-          No se encontraron resultados para la búsqueda ingresada. Por favor,
-          intente con otros valores.
-        </p>
-      ) : ApiUser?.contentFormat.max_size == 0 ? (
-        <p
-          className="pl-3 rounded-lg  bg-orange-100 
-         min-h-16 text-start items-center flex flex-row gap-1 text-lg font-semibold text-darkGrayOption-0  "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-16 md:size-8  text-orange-600 sm:self-center  "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-            />
-          </svg>
-          No se encontraron resultados para la búsqueda ingresada. Por favor,
-          intente con otros valores.
-        </p>
-      ) : (
+    <div className="p-4">
+      {!ApiUser || isLoading || isError ? (
         <div>
-          <div className="flex flex-col md:flex-row items-center md:justify-between">
+          {" "}
+          <div className=" flex flex-row bg-lightCyanShift-0 dark:bg-inherit dark:outline dark:outline-cyan-700 mt-2 mb-2 rounded-lg p-2 ">
+            <span className="mr-1">
+              <CustomButton imageName="info" />
+            </span>
             <p>
-              Acordada(s)/Resolucion(es) encontrada(s):
-              {ApiUser?.contentFormat.max_size}
+              Bienvenido a la consulta de resoluciones y acordadas. Para buscar,
+              por favor ingrese datos en, al menos, un filtro de búsqueda arriba
+              de este mensaje. Paso siguiente, dé click al botón BUSCAR.
             </p>
-            <div className="flex flex-row gap-4">
-              <p>Mostar</p>
-              <select
-                className="select outline"
-                id="Select-PageSize"
-                value={newPageSize}
-                onChange={(e) => {
-                  handleUpdatePageSize(e.currentTarget.value);
-                }}
-              >
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-              </select>
+          </div>
+          <div className="flex flex-row bg-lightCyanShift-0 dark:bg-inherit dark:outline dark:outline-cyan-700 mt-2 mb-2 rounded-lg p-2 ">
+            <span className="mr-1">
+              <CustomButton imageName="info" />
+            </span>
+            <div className="flex flex-col ">
+              <span>
+                {" "}
+                Para realizar búsquedas por texto, tiene dos opciones:
+              </span>
+              <span>
+                {" "}
+                1. Utiliza comillas (" ") para buscar una coincidencia exacta de
+                una cadena literal.
+              </span>
+              <span>
+                {" "}
+                2. Escriba palabras por separado (sin comillas) para buscar
+                todas esas palabras en cualquier orden.
+              </span>
             </div>
           </div>
-          <table className="max-sm:hidden max-md:hidden mt-2 pt-4 pb-2 block...">
-            <thead>
-              <tr>
-                <td>Número</td>
-                <td>Fecha</td>
-                <td>Descripción</td>
-                <td>Tipo</td>
-                <td>Acciones</td>
-              </tr>
-            </thead>
-            <tbody>
-              {ApiUser?.agreementsFormat.map((ApiResult, index: number) => {
-                return (
-                  <tr key={ApiResult.id}>
-                    <td>
-                      {ApiResult.agreement_number}/{ApiResult.agreement_year}
-                    </td>
-
-                    <td>{ApiResult.agreement_date.toString()}</td>
-                    <td className=" truncate overflow-hidden whitespace-nowrap">
-                      {ApiResult.agreement_description}
-                    </td>
-                    <td>{ApiResult.type_agreement.description}</td>
-
-                    <td>
-                      <button
-                        className="btn btn-circle"
-                        onClick={() => HandleButtonPress(ApiResult.id, index)}
-                      />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          <Cards ApiUser={ApiUser} HandleButtonPress={HandleButtonPress} />
-          <Modal isOpen={isOpen} toggleModal={toggleModal}></Modal>
-
-          <PageNavigationButtonsBottom
-            lastPageNumber={ApiUser?.contentFormat.max_page}
-          />
         </div>
+      ) : (
+        <>
+          {isLoading ? (
+            <div className="flex flex-col ">
+              <span className="loading loading-dots w-32 self-center text-lightBlueShift-0 "></span>
+            </div>
+          ) : isError ? (
+            <p className="pl-3 rounded-lg  bg-red-100 text-darkGrayOption-0 min-h-16 text-start items-center flex flex-row gap-1 text-lg font-semibold ">
+              <CustomButton imageName="error" />
+              No se encontraron resultados para la búsqueda ingresada. Por
+              favor, intente con otros valores.
+            </p>
+          ) : ApiUser?.contentFormat.max_size == 0 ? (
+            <p
+              className="pl-3 rounded-lg  bg-orange-100 
+         min-h-16 text-start items-center flex flex-row gap-1 text-lg font-semibold text-darkGrayOption-0  "
+            >
+              <CustomButton imageName="warning" />
+              No se encontraron resultados para la búsqueda ingresada. Por
+              favor, intente con otros valores.
+            </p>
+          ) : (
+            <div>
+              <div className="flex flex-col md:flex-row items-center md:justify-between mt-4">
+                <p className="text-2xl">
+                  Acordada(s)/Resolucion(es) encontrada(s):
+                  {ApiUser?.contentFormat.max_size}
+                </p>
+                <div className="flex flex-row gap-4  items-center ml-auto   mt-4  md:mt-0 ">
+                  <p className="text-lg">Mostrar</p>
+                  <select
+                    className="select outline bg-transparent focus:bg-transparent   outline-darkgrayOption-0 outline-1    hover:text-inherit hover:bg-inherit overflow-scroll font-semibold"
+                    id="Select-PageSize"
+                    value={newPageSize}
+                    onChange={(e) => {
+                      handleUpdatePageSize(e.currentTarget.value);
+                    }}
+                  >
+                    <option value="10" className="bg-inherit">
+                      10
+                    </option>
+                    <option value="20" className="bg-inherit">
+                      20
+                    </option>
+                    <option value="30" className="bg-inherit">
+                      30
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <table className="max-sm:hidden max-md:hidden mt-4 pt-4  block...  table-fixed table w-full  rounded-md overflow-hidden pb-4">
+                <thead>
+                  <tr className="font-bold text-lg bg-darkgrayOption-0 dark:text-gray-50  ">
+                    <td className="w-1/6 ">Número</td>
+                    <td className="w-1/6">Fecha</td>
+                    <td className="w-3/4 truncate overflow-hidden ">
+                      Descripción
+                    </td>
+                    <td className="w-1/6">Tipo</td>
+                    <td className="w-1/6">Acciones</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ApiUser?.agreementsFormat.map((ApiResult, index: number) => {
+                    return (
+                      <tr
+                        key={ApiResult.id}
+                        onClick={() => HandleButtonPress(ApiResult.id, index)}
+                        className="bg-white dark:bg-darkGrayOption-0 dark:text-gray-50 text-black"
+                      >
+                        <td className="w-1/6 ">
+                          {ApiResult.agreement_number}/
+                          {ApiResult.agreement_year}
+                        </td>
+
+                        <td className="w-1/6 ">
+                          {ApiResult.agreement_date.toString()}
+                        </td>
+                        <td className="w-3/4 truncate overflow-hidden ">
+                          {ApiResult.agreement_description}
+                        </td>
+                        <td className="w-1/6 ">
+                          {ApiResult.type_agreement.description}
+                        </td>
+
+                        <td className="w-1/6 ">
+                          <button
+                            className="btn btn-circle btn-ghost"
+                            onClick={() =>
+                              HandleButtonPress(ApiResult.id, index)
+                            }
+                          >
+                            <CustomButton imageName="eye" />
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              <Cards ApiUser={ApiUser} HandleButtonPress={HandleButtonPress} />
+              <Modal isOpen={isOpen} toggleModal={toggleModal}></Modal>
+
+              <PageNavigationButtonsBottom
+                lastPageNumber={ApiUser?.contentFormat.max_page}
+              />
+            </div>
+          )}
+        </>
       )}
     </div>
   );
--- src/Components/PageNavigationButtons/PageNavigationButtonsBottom.tsx
+++ src/Components/PageNavigationButtons/PageNavigationButtonsBottom.tsx
@@ -13,65 +13,69 @@ const PageNavigationButtonsBottom = (props: Props) => {
 
   const handleUpdateIndex = (index: number) => {
     newPageIndex(index);
-    console.log(currentIndex);
   };
 
   return (
-    <div>
-      {!(currentIndex == 0) ? (
-        <button
-          className="btn btn-circle "
-          onClick={() => handleUpdateIndex(0)}
-        >
-          <CustomButton imageName="doubleArrowLeft" />
-        </button>
-      ) : (
-        <button className="btn btn-circle disabled bg-red-300 ">
-          <CustomButton imageName="doubleArrowLeft" />
-        </button>
-      )}
-      {currentIndex == 0 ? (
-        <button className="btn btn-circle">
-          {" "}
-          <CustomButton imageName="arrowLeft" />
-        </button>
-      ) : (
-        <button
-          className="btn btn-circle"
-          onClick={() => handleUpdateIndex(currentIndex - 1)}
-        >
-          {" "}
-          <CustomButton imageName="arrowLeft" />
-        </button>
-      )}
-      {!lastPageNumber || currentIndex == lastPageNumber - 1 ? (
-        <button className="btn btn-circle disabled">
-          {" "}
-          <CustomButton imageName="arrowRight" />
-        </button>
-      ) : (
-        <button
-          className="btn btn-circle"
-          onClick={() => handleUpdateIndex(currentIndex + 1)}
-        >
-          {" "}
-          <CustomButton imageName="arrowRight" />
-        </button>
-      )}
+    <div className="flex flex-row justify-between mt-4 mb-2">
+      <div className="flex flex-row gap-2">
+        {!(currentIndex == 0) ? (
+          <button
+            className="btn btn-circle outline  btn-ghost"
+            onClick={() => handleUpdateIndex(0)}
+          >
+            <CustomButton imageName="doubleArrowLeft" />
+          </button>
+        ) : (
+          <button className="btn btn-circle  disabled btn-ghost   ">
+            <CustomButton imageName="doubleArrowLeft" />
+          </button>
+        )}
+        {currentIndex == 0 ? (
+          <button className="btn btn-circle  min-w-28 md:min-w-52 btn-ghost disabled ">
+            <span>Anterior</span>
+            <CustomButton imageName="arrowLeft" />
+          </button>
+        ) : (
+          <button
+            className="btn btn-circle min-w-28  md:min-w-52 btn-ghost outline"
+            onClick={() => handleUpdateIndex(currentIndex - 1)}
+          >
+            <span>Anterior</span>
+            <CustomButton imageName="arrowLeft" />
+          </button>
+        )}
+      </div>
+      <div className="flex flex-row gap-2">
+        {!lastPageNumber || currentIndex == lastPageNumber - 1 ? (
+          <button className="btn btn-circle btn-ghost min-w-28   disabled md:min-w-52 ">
+            <span>Siguiente</span>
+            <CustomButton imageName="arrowRight" />
+          </button>
+        ) : (
+          <button
+            className="btn btn-circle min-w-28  md:min-w-52  btn-ghost outline "
+            onClick={() => handleUpdateIndex(currentIndex + 1)}
+          >
+            {" "}
+            <span>Siguiente</span>
+            <CustomButton imageName="arrowRight" />
+          </button>
+        )}
 
-      {!lastPageNumber || currentIndex == lastPageNumber - 1 ? (
-        <button className="btn btn-circle disabled  bg-red-300">
-          <CustomButton imageName="doubleArrowRight" />
-        </button>
-      ) : (
-        <button
-          className="btn btn-circle "
-          onClick={() => handleUpdateIndex(lastPageNumber - 1)}
-        >
-          {" "}
-          <CustomButton imageName="doubleArrowRight" />
-        </button>
-      )}
+        {!lastPageNumber || currentIndex == lastPageNumber - 1 ? (
+          <button className="btn btn-circle   disabled btn-ghost  ">
+            <CustomButton imageName="doubleArrowRight" />
+          </button>
+        ) : (
+          <button
+            className="btn btn-circle   btn-ghost outline "
+            onClick={() => handleUpdateIndex(lastPageNumber - 1)}
+          >
+            {" "}
+            <CustomButton imageName="doubleArrowRight" />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
